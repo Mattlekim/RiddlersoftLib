e725d336a9a8c018ea6294ee453c87bfc72672bf
Matt <m>
2020-03-14 14:01:00 +0000

    added new stuff

--- Core/Line.cs
+++ Core/Line.cs
@@ -16,5 +16,56 @@ namespace Riddlersoft.Core
             Start = start;
             End = end;
         }
+
+        public Vector2? Intersects(Rectangle rectangle)
+        {
+            Vector2 s, e;
+            s = new Vector2(rectangle.X, rectangle.Y);
+            e = new Vector2(rectangle.X + rectangle.Width, rectangle.Y + rectangle.Height);
+            Vector2? r = Intersects(s, new Vector2(e.X, s.Y), Start, End);
+            if (r== null)
+                r = Intersects(s, new Vector2(s.X, e.Y), Start, End);
+            if (r == null)
+                r = Intersects(new Vector2(e.X, s.Y), new Vector2(e.X, e.Y), Start, End);
+            if (r == null)
+                r = Intersects(new Vector2(s.X, s.Y), new Vector2(e.X, e.Y), Start, End);
+            return r;
+        }
+
+        public Vector2? Intersects(Line l1)
+        {
+            return Intersects(l1.Start, l1.End, Start, End);
+        }
+
+        public static Vector2? Intersects(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 p4)
+        {
+            // Get the segments' parameters.
+            float dx12 = p2.X - p1.X;
+            float dy12 = p2.Y - p1.Y;
+            float dx34 = p4.X - p3.X;
+            float dy34 = p4.Y - p3.Y;
+
+            // Solve for t1 and t2
+            float denominator = (dy12 * dx34 - dx12 * dy34);
+
+            float t1 =
+                ((p1.X - p3.X) * dy34 + (p3.Y - p1.Y) * dx34)
+                    / denominator;
+            if (float.IsInfinity(t1))
+            {
+                // The lines are parallel (or close enough to it).
+                return null;
+            }
+            float t2 =
+                ((p3.X - p1.X) * dy12 + (p1.Y - p3.Y) * dx12)
+                    / -denominator;
+
+            // The segments intersect if t1 and t2 are between 0 and 1.
+            if ((t1 >= 0) && (t1 <= 1) &&
+                 (t2 >= 0) && (t2 <= 1))
+                return new Vector2(p1.X + dx12 * t1, p1.Y + dy12 * t1);
+
+            return null;
+        }
     }
 }
---
+++ Core/PathFollower.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Xna.Framework;
+
+namespace Riddlersoft.Core
+{
+    /// <summary>
+    /// my pathfollower code
+    /// this will work with as many points as needed
+    /// </summary>
+    public class PathFollower
+    {
+        public List<Vector2> Points { get; private set; }
+
+        /// <summary>
+        /// is true when it compleates the path
+        /// </summary>
+        public bool Compleated { get; private set; }
+
+        /// <summary>
+        /// the total time to compleate the path
+        /// </summary>
+        private float _timeToCompleatePath = 0;
+
+        /// <summary>
+        /// the current time
+        /// </summary>
+        private float _currentTime = 0;
+
+        /// <summary>
+        /// a buffer of each lerped point
+        /// </summary>
+        private Vector2[] _buffers;
+
+        /// <summary>
+        /// the time each split should be
+        /// </summary>
+        private float _timeSplit;
+
+        /// <summary>
+        /// how may parts its made up of
+        /// </summary>
+        private float _parts;
+
+        /// <summary>
+        /// creates the path follower with the time to compleate
+        /// </summary>
+        /// <param name="time">how long to compleate the path</param>
+        public PathFollower(float time)
+        {
+            _timeToCompleatePath = time;
+            Points = new List<Vector2>();
+        }
+
+        /// <summary>
+        /// adds a new point to the path
+        /// </summary>
+        /// <param name="point"></param>
+        public void AddPoint(Vector2 point)
+        {
+            Points.Add(point);
+        }
+
+        /// <summary>
+        /// creates the buffer call this after your last point has been added
+        /// </summary>
+        public void CreateBuffer()
+        {
+            ///we need to buffer
+            _buffers = new Vector2[Points.Count - 1];
+            _buffers[0]= Points[0];
+            for (int i = 0; i < Points.Count - 2; i++)
+            {
+                Vector2 v1 = Vector2.Lerp(_buffers[i], Points[i + 1], .5f);
+                Vector2 v2 = Vector2.Lerp(Points[i + 1], Points[i + 2], .5f);
+                _buffers[i + 1] = Vector2.Lerp(v1, v2, .5f);
+            }
+            _parts = Points.Count - 1;
+            _timeSplit = (_timeToCompleatePath / _parts) * 2f;
+            lastPos = Points[0];
+        }
+
+        public void Reset()
+        {
+            _currentTime = 0;
+            Compleated = false;
+            lastPos = Points[0];
+        }
+
+
+        public Vector2 moveDir { get; private set; }
+
+        private Vector2 lastPos = Vector2.Zero;
+        public Vector2 Update(float dt)
+        {
+
+            if (Compleated)
+            {
+                moveDir = Vector2.Zero;
+                lastPos = Points[Points.Count - 1];
+                return Points[Points.Count - 1];
+            }
+            _currentTime += dt;
+
+            if (_currentTime > _timeToCompleatePath)
+            {
+                Compleated = true;
+                moveDir = Vector2.Zero;
+                lastPos = Points[Points.Count - 1];
+                return Points[Points.Count - 1];
+            }
+
+            float halfSplit = _timeSplit * .5f;
+
+            int index = (int)Math.Floor(_currentTime / halfSplit);
+
+            float per = (float)Math.Round((_currentTime - (index * halfSplit)) / (_timeSplit), 2);
+
+            if (index + 3 < Points.Count)
+            {
+                Vector2 l1 = Vector2.Lerp(_buffers[index], Points[index + 1], per);
+                Vector2 l2 = Vector2.Lerp(Points[index + 1], Points[index + 2], per);
+
+                Vector2 l3 = Vector2.Lerp(l1, l2, per);
+                moveDir = l3 - lastPos;
+                lastPos = l3;
+                return l3;
+            }
+
+            float lasthalf = _timeToCompleatePath - _timeSplit;
+            //per = 0f;
+            per = (float)Math.Round((_currentTime - lasthalf) / (_timeSplit), 2);
+            Vector2 f1 = Vector2.Lerp(_buffers[_buffers.Length - 2], Points[Points.Count - 2], per);
+            Vector2 f2 = Vector2.Lerp(Points[Points.Count - 2], Points[Points.Count - 1], per);
+
+            Vector2 f3 = Vector2.Lerp(f1, f2, per);
+            moveDir = f3 - lastPos;
+            lastPos = f3;
+            return f3;
+        }
+
+        public void Start(float timeToCompleate)
+        {
+            _timeToCompleatePath = timeToCompleate;
+            _currentTime = 0;
+        }
+    }
+}
